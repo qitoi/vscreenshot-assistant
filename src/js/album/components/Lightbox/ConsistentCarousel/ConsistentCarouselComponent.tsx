0656b8cab551f1c4d225710b658ed87e0ec69d30
qitoi <asacly@gmail.com>
2023-02-26 06:01:51 +0900

    update Lightbox ConsistentCarouselの処理を追加

--- src/js/album/components/Lightbox/ConsistentCarousel/ConsistentCarouselComponent.tsx
+++ src/js/album/components/Lightbox/ConsistentCarousel/ConsistentCarouselComponent.tsx
@@ -1,6 +1,21 @@
 /*
+ *  Copyright 2023 qitoi
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
  *  Original Source Code
- *  https://github.com/igordanchenko/yet-another-react-lightbox/blob/v2.4.3/src/core/modules/Carousel.tsx
+ *  https://github.com/igordanchenko/yet-another-react-lightbox/blob/v2.6.1/src/core/modules/Carousel.tsx
  *
  *  MIT License
  *
@@ -27,14 +42,15 @@
 
 import * as React from 'react';
 
-import { ComponentProps, ContainerRect, Slide } from '../../types.js';
-import { createModule } from '../config.js';
-import { clsx, composePrefix, cssClass, cssVar, isImageSlide, parseLengthPercentage } from '../utils.js';
-import { ImageSlide } from '../components/index.js';
-import { useController } from './Controller.js';
-import { useEvents } from '../contexts/Events.js';
-import { useLightboxState } from '../contexts/LightboxState.js';
-import { CLASS_FLEX_CENTER, CLASS_FULLSIZE, MODULE_CAROUSEL, YARL_EVENT_BACKDROP_CLICK } from '../consts.js';
+import { ComponentProps, ContainerRect, Slide } from 'yet-another-react-lightbox';
+import { clsx, composePrefix, cssClass, cssVar, isImageSlide, parseLengthPercentage } from 'yet-another-react-lightbox/core';
+import { ImageSlide } from 'yet-another-react-lightbox/core';
+import { useController } from 'yet-another-react-lightbox/core';
+import { useEvents } from 'yet-another-react-lightbox/core';
+import { useLightboxState } from 'yet-another-react-lightbox/core';
+import { CLASS_FLEX_CENTER, CLASS_FULLSIZE, MODULE_CAROUSEL, YARL_EVENT_BACKDROP_CLICK } from 'yet-another-react-lightbox/core';
+
+import { Component } from 'yet-another-react-lightbox';
 
 function cssPrefix(value?: string) {
     return composePrefix(MODULE_CAROUSEL, value);
@@ -102,7 +118,6 @@ function CarouselSlide({ slide, offset, rect }: CarouselSlideProps) {
     };
 
     return (
-        // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
         <div
             ref={containerRef}
             className={clsx(
@@ -121,9 +136,48 @@ function Placeholder() {
     return <div className={cssClass('slide')} />;
 }
 
-export function Carousel({ slides, carousel: { finite, preload, padding, spacing } }: ComponentProps) {
+
+type ConsistentCarouselState = {
+    prevSlides: Slide[],
+    prevGlobalIndex: number,
+    prevKey: any,
+    loop: number,
+};
+
+
+export const ConsistentCarouselComponent: Component = ({ slides, carousel: { finite, preload, padding, spacing } }: React.PropsWithChildren<ComponentProps>) => {
     const { currentIndex, globalIndex } = useLightboxState().state;
     const { setCarouselRef, containerRect } = useController();
+
+
+    const { dispatch } = useLightboxState();
+    const [state, setState] = React.useState<ConsistentCarouselState>({ prevSlides: slides, prevGlobalIndex: globalIndex, prevKey: null, loop: 0 });
+
+
+    React.useEffect(() => {
+        // スライドのリストが変更された場合は元のスライドを表示するようにLightboxの表示位置を移動
+        if (slides[currentIndex].key !== state.prevSlides[currentIndex].key) {
+            const slideKeepIndex = slides.findIndex(v => v.key === state.prevKey);
+            if (slideKeepIndex !== -1) {
+                const diff = slideKeepIndex - currentIndex;
+                dispatch({ increment: diff, duration: 0 });
+                setState({ ...state, prevSlides: slides, prevGlobalIndex: globalIndex });
+            }
+        }
+        // スライドの移動を検出した場合は状態を更新
+        else {
+            const currentKey = slides[currentIndex].key;
+            const direction = Math.sign(globalIndex - state.prevGlobalIndex);
+            let loopDiff = 0;
+            if ((direction < 0 && currentIndex == (slides.length - 1)) || (direction > 0 && currentIndex == 0)) {
+                loopDiff = direction;
+            }
+            if (state.prevKey !== currentKey || loopDiff !== 0) {
+                setState({ ...state, prevSlides: slides, prevGlobalIndex: globalIndex, prevKey: currentKey, loop: state.loop + loopDiff });
+            }
+        }
+    }, [slides, currentIndex, globalIndex, state, dispatch]);
+
 
     const spacingValue = parseLengthPercentage(spacing);
     const paddingValue = parseLengthPercentage(padding);
@@ -136,32 +190,51 @@ export function Carousel({ slides, carousel: { finite, preload, padding, spacing
         height: Math.max(containerRect.height - 2 * paddingPixels, 0),
     };
 
-    const items = [];
+    // スライドのリストが変更され、表示するスライドの index がずれた場合は同じスライドを表示し続けるため補正する
+    let index = currentIndex;
+    if (slides[currentIndex].key !== state.prevSlides[currentIndex].key) {
+        const slideKeepIndex = slides.findIndex(v => v.key === state.prevKey);
+        if (slideKeepIndex !== -1) {
+            index = slideKeepIndex;
+        }
+    }
+
+    // globalIndex の差分からスライドを検出、ループの境界位置であればループ数を補正する
+    const direction = Math.sign(globalIndex - state.prevGlobalIndex);
+    let loopDiff = 0;
+    if ((direction < 0 && currentIndex == (slides.length - 1)) || (direction > 0 && currentIndex == 0)) {
+        loopDiff = direction;
+    }
+    const loop = state.loop + loopDiff;
+
+    const items: JSX.Element[] = [];
 
     if (slides?.length > 0) {
-        for (let i = currentIndex - preload; i < currentIndex; i += 1) {
-            const key = globalIndex + i - currentIndex;
+        for (let i = index - preload; i < index; i += 1) {
+            const slide = slides[(i + preload * slides.length) % slides.length];
+            const suffix = (i < 0) ? loop - 1 : loop;
+            const key = `${slide.key}:${suffix}`;
             items.push(
                 !finite || i >= 0 ? (
-                    <CarouselSlide
-                        key={key}
-                        slide={slides[(i + preload * slides.length) % slides.length]}
-                        rect={rect}
-                        offset={i - currentIndex}
-                    />
+                    <CarouselSlide key={key} slide={slide} rect={rect} offset={i - index} />
                 ) : (
                     <Placeholder key={key} />
                 )
             );
         }
 
-        items.push(<CarouselSlide key={globalIndex} slide={slides[currentIndex]} rect={rect} offset={0} />);
+        {
+            const key = `${slides[index].key}:${loop}`;
+            items.push(<CarouselSlide key={key} slide={slides[index]} rect={rect} offset={0} />);
+        }
 
-        for (let i = currentIndex + 1; i <= currentIndex + preload; i += 1) {
-            const key = globalIndex + i - currentIndex;
+        for (let i = index + 1; i <= index + preload; i += 1) {
+            const slide = slides[i % slides.length];
+            const suffix = (i >= slides.length) ? loop + 1 : loop;
+            const key = `${slide.key}:${suffix}`;
             items.push(
                 !finite || i <= slides.length - 1 ? (
-                    <CarouselSlide key={key} slide={slides[i % slides.length]} rect={rect} offset={i - currentIndex} />
+                    <CarouselSlide key={key} slide={slide} rect={rect} offset={i - index} />
                 ) : (
                     <Placeholder key={key} />
                 )
@@ -186,6 +259,4 @@ export function Carousel({ slides, carousel: { finite, preload, padding, spacing
             {items}
         </div>
     );
-}
-
-export const CarouselModule = createModule(MODULE_CAROUSEL, Carousel);
+};
